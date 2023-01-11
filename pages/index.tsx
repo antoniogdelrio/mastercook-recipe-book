8a4b926b732f5ad044b20156304235903bbd1b4f
antoniogdelrio <antoniogdelrio@hotmail.com>
2023-01-11 20:30:30 -0300

    feat: updates in the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,67 @@
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import { useState } from "react";
 import Header from "../components/Header/Header";
-import RecipeCard from "../components/RecipeCard/RecipeCard";
+import Pagination from "../components/Pagination/Pagination";
+import { RecipeCardSchema } from "../components/RecipeCard/RecipeCard";
 import RecipeList from "../components/RecipeList/RecipeList";
+import SkeletonCards from "../components/SkeletonCards/SkeletonCards";
+import { RECIPES_URL } from "../constants/apiUrls";
+import useRecipes from "../hooks/useRecipes";
 
-export default function Home() {
-  const mock = [
-    {
-      difficulty: "Hard",
-      image: "/temaki.jpg",
-      title: "Temaki",
-      time: 120,
-    },
-    {
-      difficulty: "Hard",
-      image: "/temaki.jpg",
-      title: "Temaki",
-      time: 120,
-    },
-    {
-      difficulty: "Hard",
-      image: "/temaki.jpg",
-      title: "Temaki",
-      time: 120,
-    },
-    {
-      difficulty: "Hard",
-      image: "/temaki.jpg",
-      title: "Temaki",
-      time: 120,
-    },
-  ]
+interface HomeProps {
+  recipes: RecipeCardSchema[],
+  totalItems: number,
+  queryPage: number
+}
+
+export default function Home({ recipes, totalItems, queryPage } : HomeProps) {
+  const [page, setPage] = useState(queryPage)
+  const router = useRouter()
+
+  const { data: paginatedRecipes, isLoading } = useRecipes(Number(page), queryPage !== page)
+
+  const handleChangePage = (delta : number) => () => {
+    const newPage : number = Number(page) + delta
+    setPage(newPage)
+    router.query.page = `${newPage}`
+    router.push(router)
+  }
+
+  console.log(queryPage === page, queryPage, page, recipes, isLoading)
+
+  const cards = queryPage === page ? recipes : paginatedRecipes
 
   return (
     <>
       <Header/>
-      <RecipeList cards={mock} />
+      {
+        isLoading ?
+        <SkeletonCards quantity={9} /> :
+        <RecipeList cards={cards} />
+      }
+      <Pagination
+        goNextPage={handleChangePage(+1)}
+        goPreviousPage={handleChangePage(-1)}
+        currentPage={page}
+        pageSize={9}
+        totalItems={totalItems}
+      />
     </>
   )
+}
+
+export const getServerSideProps : GetServerSideProps = async (context) =>  {
+  const queryPage = Number(context.query.page) || 1
+  const res = await fetch(`${RECIPES_URL}?_page=${queryPage}&_limit=9`)
+  const totalItems = res.headers.get('X-Total-Count')
+  const recipes = await res.json()
+
+  return {
+    props: {
+      recipes,
+      totalItems,
+      queryPage
+    }
+  }
 }
