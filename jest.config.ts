3973c4ff01868372e8e9af23e988286a15ad1235
antoniogdelrio <antoniogdelrio@hotmail.com>
2023-01-14 00:07:33 -0300

    test: setup unit tests

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+module.exports = createJestConfig(customJestConfig)
