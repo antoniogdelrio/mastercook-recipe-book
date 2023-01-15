918cee911b53affb81df9398c64221c99122c4cf
antoniogdelrio <antoniogdelrio@hotmail.com>
2023-01-15 00:03:24 -0300

    fix: changed jest config to include untested files in the coverage report

--- jest.config.ts
+++ jest.config.ts
@@ -9,6 +9,20 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!<rootDir>/out/**',
+    '!<rootDir>/.next/**',
+    '!<rootDir>/*.config.js',
+    '!<rootDir>/coverage/**',
+  ],
+  coveragePathIgnorePatterns: [
+    "node_modules",
+    "test"
+  ]
 }
 
 module.exports = createJestConfig(customJestConfig)
