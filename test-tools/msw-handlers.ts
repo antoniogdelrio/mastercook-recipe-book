c11214b0a778d2bfe1d8a7fffb6b22d9073c9388
antoniogdelrio <antoniogdelrio@hotmail.com>
2023-01-22 22:47:00 -0300

    fix: removed logs

--- test-tools/msw-handlers.ts
+++ test-tools/msw-handlers.ts
@@ -5,16 +5,13 @@ import { RecipeMock, RecipesMock } from './mocks/Recipes'
 
 const handlers = [
   rest.get(`${RECIPES_URL}/:id`, (_req, res, ctx) => {
-    console.log('rodando o primeiro mock')
     return res(
       ctx.status(200),
       ctx.json(RecipeMock)
     )
   }),
   rest.get(`${RECIPES_URL}`, (req, res, ctx) => {
-    console.log('the second mock is running')
     if (req.url.searchParams.get('q')) {
-      console.log('rodando mock com busca')
       return res(
         ctx.delay(500),
         ctx.status(200),
